7836914b6aa235ebae609de16acec96dd86f0952
NMA93 <mail@nilsmaeder.ch>
2018-09-09 16:02:03 +0200

    anpassungen_clicker_items

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 let doener = document.getElementById("doener")
 let item = document.getElementById("item")
+let item2 = document.getElementById("item2")
+let item3 = document.getElementById("item3")
 let counter = document.getElementById("counter")
 let budgetcounter = document.getElementById("budgetcounter")
 
@@ -17,19 +19,53 @@ let doenerclick = function(e){
         if(budget >= 190) {
             item.classList.remove("hidden");
         }
+        setInterval(function() {
+            if(budget < 190) {
+                item.classList.add("hidden");
+            }
+        }, 100);
+        
+        if(budget >= 290) {
+            item2.classList.remove("hidden");
+        }
+        setInterval(function() {
+            if(budget < 290) {
+                item2.classList.add("hidden");
+            }
+        }, 100);
+
+        if(budget >= 390) {
+            item3.classList.remove("hidden");
+        }
+        setInterval(function() {
+            if(budget < 390) {
+                item3.classList.add("hidden");
+            }
+        }, 100);
 }
 
 let minus200 = function(e){
     budget = budget - 200;
+    budgetcounter.innerHTML = budget;  
+}
+
+let minus300 = function(e){
+    budget = budget - 300;
     budgetcounter.innerHTML = budget;
-    if(budget < 190) {
-        item.classList.add("hidden");
-    }   
+    
+}
+
+let minus400 = function(e){
+    budget = budget - 400;
+    budgetcounter.innerHTML = budget;
+    
 }
 
 
 doener.addEventListener("click", doenerclick);
 item.addEventListener("click", minus200);
+item2.addEventListener("click", minus300);
+item3.addEventListener("click", minus400);
 doener.addEventListener("click", budgetcount);
 
 /* 
